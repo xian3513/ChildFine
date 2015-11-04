bac0adbca2510eba4148d7b6f1f7a2213e3e5c5f
zhangzx <23090294@qq.com>
2015-11-04 17:48:18 +0800

    log

--- ChildFine/Main/Learn/Controller/LearnViewController.m
+++ ChildFine/Main/Learn/Controller/LearnViewController.m
@@ -23,7 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"learnViewController:%@",self.view);
+    
 }
 
 - (void)buttonPress:(UIBarButtonItem *)item {
